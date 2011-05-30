b0be14c7027d29358067df83944f8e2129450050
myungdragon@gmail.com <myungdragon@gmail.com@dd84ee21-ef33-6ea9-7448-4e4c21b60c5c>
2011-05-30 18:04:56 +0000

    외부 태그 예제 추가(ex_cloud.xml) thread_list 에 and, or, not 연산 추가

--- trunk/Server/php/thread_list.php
+++ trunk/Server/php/thread_list.php
@@ -4,15 +4,14 @@ include ("connection.php");
 // Parameters
 $req_device_id = $_GET["device_id"];
 $req_document_srl = $_GET["document_srl"];
-$req_tag_srl_list = $_GET["tag_srl_list"];
+
+// Search Parameters
+$req_tag_and_list = $_GET["and"];
+$req_tag_or_list = $_GET["or"];
+$req_tag_not_list = $_GET["not"];
 
 // Fetch Thread List
-$query = "SELECT DISTINCT documents.* FROM document_tags LEFT OUTER JOIN documents ON document_tags.document_srl = documents.document_srl WHERE 1=1 ";
-
-// 태그 리스트에 포함된 글들 모두 포함
-if ($req_tag_srl_list) {
-	$query.= " AND document_tags.tag_srl IN (".$req_tag_srl_list.")";
-}
+$query = "SELECT documents.*, tag_srls FROM documents LEFT OUTER JOIN (SELECT document_srl, GROUP_CONCAT(tag_srl) tag_srls FROM document_tags GROUP BY document_srl) AS tbl_tag_srls ON documents.document_srl = tbl_tag_srls.document_srl WHERE 1=1 ";
 
 // 자기가 남긴 글 보기
 if ($req_device_id) {
@@ -28,7 +27,43 @@ $query.= " ORDER BY timestamp DESC";
 //echo $query;
 $result = mysql_query($query, $connect) or die(" : ".mysql_error());
 
-// XML Output
+// Not 배열
+$array_name_list = explode(",", $req_tag_not_list);
+$array_srl_not_list = array();
+foreach ($array_name_list as $key=>$val) {
+	$query_tag = "SELECT tag_srl FROM tags WHERE name='$val'";
+	$result_tag = mysql_query($query_tag, $connect) or die("error".__LINE__);
+	$row_tag = mysql_fetch_array($result_tag);
+	$tag_srl = $row_tag[tag_srl];
+	array_push($array_srl_not_list, $tag_srl);
+}
+
+// or 배열
+$array_name_list = explode(",", $req_tag_or_list);
+$array_srl_or_list = array();
+foreach ($array_name_list as $key=>$val) {
+	$query_tag = "SELECT tag_srl FROM tags WHERE name='$val'";
+	$result_tag = mysql_query($query_tag, $connect) or die("error".__LINE__);
+	$row_tag = mysql_fetch_array($result_tag);
+	$tag_srl = $row_tag[tag_srl];
+	array_push($array_srl_or_list, $tag_srl);
+}
+
+// and 배열
+$array_name_list = explode(",", $req_tag_and_list);
+$array_srl_and_list = array();
+foreach ($array_name_list as $key=>$val) {
+	$query_tag = "SELECT tag_srl FROM tags WHERE name='$val'";
+	$result_tag = mysql_query($query_tag, $connect) or die("error".__LINE__);
+	$row_tag = mysql_fetch_array($result_tag);
+	$tag_srl = $row_tag[tag_srl];
+	array_push($array_srl_and_list, $tag_srl);
+}
+//print_r($array_srl_not_list);
+//print_r($array_srl_or_list);
+//print_r($array_srl_and_list);
+
+//// XML Output
 header("Content-Type: text/xml;");
 $writer = new XMLWriter();
 $writer->openURI('php://output');
@@ -36,8 +71,43 @@ $writer->startDocument('1.0','UTF-8');
 $writer->setIndent(4);
 
 $writer->startElement('THREADLIST');
+
+// and, or, not 조건에 따라 출력 여부를 결정
 while($row=mysql_fetch_array($result))
 {
+	$is_ok = true;
+	$array_srl_list = explode(",", $row[tag_srls]);
+	
+	if ($req_tag_or_list) {
+		$is_ok = false;
+		foreach ($array_srl_or_list as $key=>$val) {
+			if (in_array($val, $array_srl_list)) {
+				$is_ok = true;
+				break; // 통과시킴
+			}
+		}
+	}
+	else if ($req_tag_and_list) { 
+		$is_ok = true;
+		foreach ($array_srl_and_list as $key=>$val) {
+			if (!in_array($val, $array_srl_list)) {
+				$is_ok = false; // 하나라도 존재 안하면 통과 안됨
+			}
+		}		
+	}
+
+	// Not 처리
+	foreach ($array_srl_not_list as $key=>$val) {	
+		// Not 리스트에 있으면 is_ok = false 가 됨
+		if (in_array($val, $array_srl_list)) {
+			$is_ok = false;
+			break; 
+		}
+	}				
+	
+	if ($is_ok == false) 
+		continue;
+	
 	$query_count = "SELECT count(*) AS comment_count FROM comments WHERE document_srl = ".$row[document_srl];
 	$result_count = mysql_query($query_count, $connect) or die("counting error : ".mysql_error());
 	$row_count = mysql_fetch_array($result_count);
@@ -53,6 +123,7 @@ while($row=mysql_fetch_array($result))
 	$writer->writeAttribute('device_id', $row[device_id]);
 	$writer->writeAttribute('content', $row[content]);
 	$writer->writeAttribute('comment_count', $row_count[comment_count]);
+	$writer->writeAttribute('tag_srls', $row[tag_srls]);	
 	$writer->endElement();
 }
 $writer->endElement();
